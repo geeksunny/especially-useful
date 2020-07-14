7b1b9497dc861b164ffb858472bd3a7fbbac2740
Justin Swanson <geeksunny@gmail.com>
2020-07-14 02:44:39 -0500

    Refactored EspNowTools into a Singleton class

--- include/EspNowTools.h
+++ include/EspNowTools.h
@@ -4,18 +4,92 @@
 #include <cstdint>
 #include <cstddef>
 
-#define ESP_NOW_SEND(dest_mac_addr, data) esp_now_tools::send(dest_mac_addr, (uint8_t *) &data, sizeof(data));
 #define ESP_NOW_GET_DATA(dest, src) memcpy(&dest, src, sizeof(dest));
 
 namespace esp_now_tools {
 
-typedef void (*esp_now_recv_cb_t)(uint8_t *mac_addr, uint8_t *data, uint8_t len);
-typedef void (*esp_now_send_cb_t)(uint8_t *mac_addr, uint8_t status);
+// These definitions were pulled from esp8266 ESP-NOW / Wifi library to avoid build issues with includes.
+//  Names and values have been tweaked to avoid name clashes.
+typedef void (*esp_now_onrecv_cb_t)(uint8_t *mac_addr, uint8_t *data, uint8_t len);
+typedef void (*esp_now_onsend_cb_t)(uint8_t *mac_addr, uint8_t status);
+enum class wifi_mode {
+  // Prevent any changes to wifi during setup
+  DONT_TOUCH,
+  // Values should align with ESP8266WiFiType.h::WiFiMode
+  STA = 1,
+  AP,
+  AP_STA
+};
 
-bool startClient(uint8_t *server_mac_addr, esp_now_send_cb_t on_send_cb);
-bool startServer(esp_now_recv_cb_t on_recv_cb);
+typedef void (*esp_now_self_repair_cb_t)(uint8_t new_channel);
 
-int send(uint8_t *dest_mac_addr, uint8_t *data, size_t length);
+// TODO: Build-in the onrecv callback as simple function to copy data to local `result` member variable;
+//  Implementor's onrecv callback would use actual result type as parameter;
+//  Callback would be triggered by a static call to `EspNow::loop();` added to main loop() function.
+//   Checks for value set to `result`, calls callback if found
+//   This approach should avoid any processing on the interrupt
+class EspNow {
+  friend class Builder;
+  static EspNow *instance;
+
+ public:
+  enum class Role {
+    // Values should align with espnow.h::esp_now_role
+    CONTROLLER = 1,
+    SLAVE,
+    COMBO,
+    MAX,
+    // Alias values
+    CLIENT = 1,
+    SERVER
+  };
+
+  class Builder {
+    friend class EspNow;
+   public:
+    Builder &setOnSendCallback(esp_now_onsend_cb_t on_send_cb);
+    Builder &setOnRecvCallback(esp_now_onrecv_cb_t on_recv_cb);
+    Builder &setWifiMode(wifi_mode wifi_mode);
+    EspNow *commit();
+   private:
+    Role role_;
+    esp_now_onsend_cb_t on_send_cb_ = nullptr;
+    esp_now_onrecv_cb_t on_recv_cb_ = nullptr;
+    wifi_mode wifi_mode_ = wifi_mode::STA;
+
+    explicit Builder(Role role) : role_(role) {}
+  };
+
+  static EspNow* getInstance();
+  static EspNow::Builder configure(Role role);
+  static bool unpairAll();
+
+  // Self-repair mode only enabled if a `self_repair_cb` is defined.
+  bool pair(uint8_t *mac_addr, uint8_t channel = 0, esp_now_self_repair_cb_t self_repair_cb = nullptr);
+  bool unpair(uint8_t *mac_addr);
+  bool send(uint8_t *dest_mac_addr, uint8_t *data, size_t length);
+
+  template<typename T>
+  bool send(uint8_t *dest_mac_addr, T data) {
+    return send(dest_mac_addr, (uint8_t *) &data, sizeof(T));
+  }
+  template<typename T>
+  bool sendToAllPeers(T data) {
+    // TODO: Does this work if peers are on different channels from one another? Will wifi auto-switch?
+    return send(nullptr, (uint8_t *) &data, sizeof(T));
+  }
+
+  explicit operator bool();
+  explicit operator bool() const;
+
+ private:
+  static EspNow* getInstance(bool config_success);
+  static bool reset();
+
+  bool configured_ = false;
+
+  EspNow() = default;
+};
 
 }
 
--- src/EspNowTools.cpp
+++ src/EspNowTools.cpp
@@ -6,53 +6,152 @@
 
 namespace esp_now_tools {
 
-bool startClient(uint8_t *server_mac_addr, esp_now_send_cb_t on_send_cb) {
-  // Init wifi in station mode.
-  WiFi.mode(WIFI_STA);
-  // Init ESP-NOW.
-  if (esp_now_init() != 0) {
-    DEBUG("Error initializing ESP-NOW")
-    return false;
+////////////////////////////////////////////////////////////////
+// Class : EspNow //////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+/* static */
+esp_now_tools::EspNow *esp_now_tools::EspNow::instance;
+
+/* static */
+EspNow *EspNow::getInstance() {
+  if (!instance) {
+    instance = new EspNow();
   }
-  // Set up role & callback.
-  if (esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER) != 0) {
-    DEBUG("Error setting ESP-NOW role.")
-    return false;
+  return instance;
+}
+
+/* static */
+EspNow::Builder EspNow::configure(EspNow::Role role) {
+  if (instance) {
+    // TODO: Should this be moved to the top of Builder::commit() ?
+    esp_now_tools::EspNow::reset();
   }
-  if (esp_now_register_send_cb(on_send_cb) != 0) {
-    DEBUG("Error registering ESP-NOW OnSend callback.")
-    return false;
+  return Builder(role);
+}
+
+/* static */
+bool EspNow::unpairAll() {
+  uint8_t *mac_addr = esp_now_fetch_peer(true);
+  while (mac_addr) {
+    esp_now_del_peer(mac_addr);
+    mac_addr = esp_now_fetch_peer(false);
+    // TODO: is `_del_peer(mac_addr)` going to throw off the result of `_fetch_peer(false)`?
+    //  May need to pass true each time....
   }
-  // Pair with the server address.
-  if (esp_now_add_peer(server_mac_addr, ESP_NOW_ROLE_SLAVE, 1, nullptr, 0) != 0) {
+  return false;
+}
+
+bool EspNow::pair(uint8_t *mac_addr, uint8_t channel, esp_now_self_repair_cb_t self_repair_cb) {
+  if (esp_now_add_peer(mac_addr, ESP_NOW_ROLE_SLAVE, channel, nullptr, 0) != 0) {
     DEBUG("Error pairing with ESP-NOW server.")
+    // Self-repair mode, only if a callback was passed.
+    if (self_repair_cb) {
+      DEBUG("Starting ESP-NOW channel self-repair procedure");
+      int peer_channel = esp_now_get_peer_channel(mac_addr);
+      if (peer_channel > 0 && peer_channel < 15) {
+        if (wifi_set_channel(peer_channel) && esp_now_set_peer_channel(mac_addr, peer_channel) == 0) {
+          // TODO: Do we need to make another call to esp_now_add_peer() here?
+          self_repair_cb(peer_channel);
+          return true;
+        }
+      }
+    }
     return false;
   }
   return true;
 }
 
-bool startServer(esp_now_recv_cb_t on_recv_cb) {
-  // Init wifi in station mode.
-  WiFi.mode(WIFI_STA);
-  // Init ESP-NOW.
+bool EspNow::unpair(uint8_t *mac_addr) {
+  return esp_now_del_peer(mac_addr) == 0;
+}
+
+bool EspNow::send(uint8_t *dest_mac_addr, uint8_t *data, size_t length) {
+  return esp_now_send(dest_mac_addr, data, length);
+}
+
+EspNow::operator bool() {
+  return configured_;
+}
+
+EspNow::operator bool() const {
+  return configured_;
+}
+
+/* static */
+EspNow *EspNow::getInstance(bool config_success) {
+  if (!instance) {
+    instance = new EspNow();
+  }
+  instance->configured_ = config_success;
+  return instance;
+}
+
+/* static */
+bool EspNow::reset() {
+  // Unpairing devices here is unnecessary; They are unpaired during deinit.
+  if (esp_now_unregister_send_cb() != 0) {
+    // todo: handle failure?
+  }
+  if (esp_now_unregister_recv_cb() != 0) {
+    // todo: handle failure?
+  }
+  if (esp_now_deinit() != 0) {
+    // todo: handle failure?
+  }
+  getInstance()->configured_ = false;
+  return true;
+}
+
+////////////////////////////////////////////////////////////////
+// Class : EspNow::Builder /////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+EspNow::Builder &EspNow::Builder::setOnSendCallback(esp_now_send_cb_t on_send_cb) {
+  on_send_cb_ = on_send_cb;
+  return *this;
+}
+
+EspNow::Builder &EspNow::Builder::setOnRecvCallback(esp_now_recv_cb_t on_recv_cb) {
+  on_recv_cb_ = on_recv_cb;
+  return *this;
+}
+
+EspNow::Builder &EspNow::Builder::setWifiMode(wifi_mode wifi_mode) {
+  wifi_mode_ = wifi_mode;
+  return *this;
+}
+
+EspNow *EspNow::Builder::commit() {
+  // Wifi Mode
+  if (wifi_mode_ != wifi_mode::DONT_TOUCH) {
+    WiFi.mode((WiFiMode_t) wifi_mode_);
+  }
+  // ESP-NOW init
   if (esp_now_init() != 0) {
     DEBUG("Error initializing ESP-NOW")
-    return false;
+    return EspNow::getInstance(false);
   }
-  // Set up role & callback.
-  if (esp_now_set_self_role(ESP_NOW_ROLE_SLAVE) != 0) {
+  // ESP-NOW role
+  if (esp_now_set_self_role((uint8_t) role_) != 0) {
     DEBUG("Error setting ESP-NOW role.")
-    return false;
+    return EspNow::getInstance(false);
   }
-  if (esp_now_register_recv_cb(on_recv_cb) != 0) {
-    DEBUG("Error registering ESP-NOW OnRecv callback.")
-    return false;
+  // ESP-NOW onSend callback
+  if (on_send_cb_) {
+    if (esp_now_register_send_cb(on_send_cb_) != 0) {
+      DEBUG("Error registering ESP-NOW OnSend callback.")
+      return EspNow::getInstance(false);
+    }
   }
-  return true;
-}
-
-int send(uint8_t *dest_mac_addr, uint8_t *data, size_t length) {
-  return esp_now_send(dest_mac_addr, data, length);
+  // ESP-NOW onRecv callback
+  if (on_recv_cb_) {
+    if (esp_now_register_recv_cb(on_recv_cb_) != 0) {
+      DEBUG("Error registering ESP-NOW OnRecv callback.")
+      return EspNow::getInstance(false);
+    }
+  }
+  return EspNow::getInstance(true);
 }
 
 }
