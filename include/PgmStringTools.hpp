20c7ea9a3e0a71b462da5bda066293c39c324b8e
Justin Swanson <geeksunny@gmail.com>
2024-12-04 15:44:03 -0600

    Case-insensitive variants of PGM string macros

--- include/PgmStringTools.hpp
+++ include/PgmStringTools.hpp
@@ -2,10 +2,14 @@
 #define ESPECIALLY_USEFUL_INCLUDE_PGMSTRINGTOOLS_HPP_
 
 #include <cstring>
+#include <strings.h>
 #include "prog_str.h"
 
 #define PGM_STR_EQ(pgm_str, c_str, c_str_len, pgms_buffer)\
   (c_str_len == sizeof(pgm_str) && std::strcmp(c_str, read_prog_str(pgm_str, pgms_buffer)) == 0)
+
+#define PGM_CASE_STR_EQ(pgm_str, c_str, c_str_len, pgms_buffer)\
+  (c_str_len == sizeof(pgm_str) && strcasecmp(c_str, read_prog_str(pgm_str, pgms_buffer)) == 0)
 
 #define STR_EQ_INIT(cstr_ptr)\
   const char *the_str = cstr_ptr;\
@@ -17,5 +21,11 @@
 
 #define STR_EQ_DO(progmem_str, code_block)\
   if (PGM_STR_EQ(progmem_str, the_str, the_str_len, the_buf)) { code_block }
+
+#define STR_CASE_EQ_RET(progmem_str, return_value)\
+  if (PGM_CASE_STR_EQ(progmem_str, the_str, the_str_len, the_buf)) { return return_value; }
+
+#define STR_CASE_EQ_DO(progmem_str, code_block)\
+  if (PGM_CASE_STR_EQ(progmem_str, the_str, the_str_len, the_buf)) { code_block }
 
 #endif //ESPECIALLY_USEFUL_INCLUDE_PGMSTRINGTOOLS_HPP_
